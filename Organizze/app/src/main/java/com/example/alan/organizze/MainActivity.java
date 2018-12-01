dc3458400d0452bd5324e1ebfa9cb732df6e2638
Jose Alan <alan123art@gmail.com>
2018-11-30 22:31:01 -0300

    Intro Activity parte 1

--- Organizze/app/src/main/java/com/example/alan/organizze/MainActivity.java
+++ Organizze/app/src/main/java/com/example/alan/organizze/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.alan.organizze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.alan.organizze.Activity.CadastroActivity;
+import com.example.alan.organizze.Activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -11,28 +16,45 @@ public class MainActivity extends IntroActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
 
         setButtonBackVisible(false);
         setButtonNextVisible(false);
-        addSlide( new FragmentSlide.Builder()
+        addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_1)
                 .build());
-        addSlide( new FragmentSlide.Builder()
+        addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_2)
                 .build());
-        addSlide( new FragmentSlide.Builder()
+        addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_3)
                 .build());
-        addSlide( new FragmentSlide.Builder()
+        addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
                 .canGoForward(false)
                 .build());
 
 
     }
+
+    public void btEntrar(View view) {
+        // Toast.makeText(this, "entrou para a globo", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(this, LoginActivity.class));
+
+    }
+
+    public void btCadastrar(View view) {
+        //Toast.makeText(this, "Cadastrou infeliz", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(this, CadastroActivity.class));
+
+    }
+
 }
