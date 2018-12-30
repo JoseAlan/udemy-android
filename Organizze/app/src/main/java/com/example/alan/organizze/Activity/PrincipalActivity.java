e1a3bab0c947a846c19adebcd1387688dfe5f6ca
Jose Alan <alan123art@gmail.com>
2018-12-30 13:26:37 -0300

    Adicionado CalendarView

--- Organizze/app/src/main/java/com/example/alan/organizze/Activity/PrincipalActivity.java
+++ Organizze/app/src/main/java/com/example/alan/organizze/Activity/PrincipalActivity.java
@@ -7,10 +7,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.alan.organizze.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private MaterialCalendarView calendarView;
+    private TextView saldo, saldacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,13 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        calendarView = findViewById(R.id.principal_calendarView);
+        saldo = findViewById(R.id.principal_saldo);
+        saldacao = findViewById(R.id.principal_saudacao);
+        configCalenadario();
+
+
 /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +50,18 @@ public class PrincipalActivity extends AppCompatActivity {
     public void adicionaDespesa(View view){
         startActivity(new Intent(this, DespesasActivity.class ));
 
+    }
+
+    public void configCalenadario(){
+        CharSequence meses [] = {"Jan", "Fev", "Mar", "Abr", "Maio" , "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView materialCalendarView, CalendarDay calendarDay) {
+
+            }
+        });
     }
 
 }
