0be729277f523b73bcdf00414180bd9761a731a8
Jose Alan <alan123art@gmail.com>
2018-12-30 13:53:57 -0300

    Adicionando Toobar

--- Organizze/app/src/main/java/com/example/alan/organizze/Activity/PrincipalActivity.java
+++ Organizze/app/src/main/java/com/example/alan/organizze/Activity/PrincipalActivity.java
@@ -6,10 +6,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.alan.organizze.R;
+import com.example.alan.organizze.config.ConfigFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
@@ -18,12 +22,14 @@ public class PrincipalActivity extends AppCompatActivity {
 
     private MaterialCalendarView calendarView;
     private TextView saldo, saldacao;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
 
         calendarView = findViewById(R.id.principal_calendarView);
@@ -43,6 +49,28 @@ public class PrincipalActivity extends AppCompatActivity {
         });*/
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_sair:
+                firebaseAuth = ConfigFirebase.getAuth();
+                firebaseAuth.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
     public void adicionaReceita (View view){
             startActivity(new Intent(this, ReceitasActivity.class));
     }
